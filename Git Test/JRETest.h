67f4c444983fb493bff76f986aefe87eed2424f1
Jeffrey Ely <jeffreyely@jeffreys-air.attlocal.net>
2014-09-01 11:34:50 -0400

    added a test class

---
+++ Git Test/JRETest.h
@@ -0,0 +1,13 @@
+//
+//  JRETest.h
+//  Git Test
+//
+//  Created by Jeffrey Ely on 9/1/14.
+//  Copyright (c) 2014 MEJREWare. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JRETest : NSObject
+
+@end
---
+++ Git Test/JRETest.m
@@ -0,0 +1,13 @@
+//
+//  JRETest.m
+//  Git Test
+//
+//  Created by Jeffrey Ely on 9/1/14.
+//  Copyright (c) 2014 MEJREWare. All rights reserved.
+//
+
+#import "JRETest.h"
+
+@implementation JRETest
+
+@end
